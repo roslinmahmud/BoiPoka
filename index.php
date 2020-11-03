e41d54fc5502b20b0431860ef8bdc08470e42df6
Roslin Mahmud Joy <roslinmahmudjoy@gmail.com>
2020-11-03 16:01:46 +0600

    clean code added in index

--- index.php
+++ index.php
@@ -23,25 +23,24 @@
     <div class="container">
 <?php
     $result = getBookData();
-    if ($result->num_rows > 0) {
-        print '<div class="row border" style="margin: 10px;">';
-        while($data = $result->fetch_assoc()){
-        print'
+    if ($result->num_rows > 0) { ?>
+        <div class="row border" style="margin: 10px;">
+  <?php while($data = $result->fetch_assoc()){ ?>
+    
             <div class="col">
               <div class="card" style="width: 192px; margin: 2px;">
-                  <img src="/BoiPoka/img/book/'.$data['BookImage'].'" class="card-img-top" alt="..." height="160px" width="160px">
+                  <img src="/BoiPoka/img/book/<?php print $data['BookImage'] ?>" class="card-img-top" alt="..." height="160px" width="160px">
                   <div class="card-body">
-                  <h6 class="card-title">'.$data['BookName'].'</h6>
-                  <p class="card-text">'.$data['AuthorName'].'</p>
-                  <a href="book.php?id='. $data['BookID'].'" class="btn btn-primary">View Details</a>
+                  <h6 class="card-title"><?php print $data['BookName'] ?></h6>
+                  <p class="card-text"><?php print $data['AuthorName'] ?></p>
+                  <a href="book.php?id=<?php print $data['BookID'] ?>" class="btn btn-primary">View Details</a>
                   </div>
               </div>
             </div>
-        ';
-        }
-        print "</div>";
-    }
-?>
+            
+  <?php } ?>
+        </div>
+<?php } ?>
     </div>
 
     <!-- Loads the Footer-->
